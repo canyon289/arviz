88df81352c27418b6a483ed5c3a43f08437a932b
Ravin Kumar <ravinsdrive@gmail.com>
2018-10-12 06:49:59 -0700

    Fix mistaken return type

--- arviz/utils.py
+++ arviz/utils.py
@@ -8,17 +8,17 @@ def _var_names(var_name):
 
     Parameters
     ----------
-    var_name: str or iter
+    var_name: str, list or None
 
     Returns
     -------
-    var_name: tuple
+    var_name: list or None
     """
     if var_name is None:
         return None
 
     elif isinstance(var_name, str):
-        return (var_name,)
+        return [var_name]
 
     else:
-        return tuple(var_name)
+        return var_name
